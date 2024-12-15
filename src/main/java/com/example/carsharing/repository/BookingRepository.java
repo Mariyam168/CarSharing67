d6c8a6536c91cf3d537db313b53b8676d1fae9c8
Mariyam168 <maruusyaaa@gmail.com>
2024-12-15 23:27:30 +0600

    bookingCreateUpdateTwo22

--- src/main/java/com/example/carsharing/repository/BookingRepository.java
+++ src/main/java/com/example/carsharing/repository/BookingRepository.java
@@ -12,13 +12,6 @@ import java.util.List;
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     List<Booking> findByUserId(Long userId);
     boolean existsByUserIdAndStatusIn(Long userId, List<BookingStatus> statuses);
-    @Query("SELECT b FROM Booking b WHERE b.user.id = :userId AND b.id <> :bookingId AND " +
-            "(b.startDate <= :endDate AND b.endDate >= :startDate)")
-    List<Booking> findOverlappingBookingsByUserIdAndExcludeBooking(@Param("userId") Long userId,
-                                                                   @Param("bookingId") Long bookingId,
-                                                                   @Param("startDate") LocalDate startDate,
-                                                                   @Param("endDate") LocalDate endDate);
-
     @Query("SELECT b FROM Booking b WHERE b.user.id = :userId AND " +
             "(b.startDate <= :endDate AND b.endDate >= :startDate)")
     List<Booking> findOverlappingBookingsByUserId(@Param("userId") Long userId,
